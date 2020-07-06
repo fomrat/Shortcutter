50d18011e1bee7d3269416ac38647f92f753629f
fomrat <fomrat@gmail.com>
2020-07-05 20:15:35 -0400

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Shortcutter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.LstFiles = new System.Windows.Forms.ListBox();
+            this.ChkOnTop = new System.Windows.Forms.CheckBox();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LstFiles
+            // 
+            this.LstFiles.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LstFiles.FormattingEnabled = true;
+            this.LstFiles.ItemHeight = 14;
+            this.LstFiles.Location = new System.Drawing.Point(0, 0);
+            this.LstFiles.Name = "LstFiles";
+            this.LstFiles.Size = new System.Drawing.Size(176, 186);
+            this.LstFiles.TabIndex = 0;
+            // 
+            // ChkOnTop
+            // 
+            this.ChkOnTop.AutoSize = true;
+            this.ChkOnTop.Location = new System.Drawing.Point(2, 196);
+            this.ChkOnTop.Name = "ChkOnTop";
+            this.ChkOnTop.Size = new System.Drawing.Size(49, 17);
+            this.ChkOnTop.TabIndex = 1;
+            this.ChkOnTop.Text = "&Float";
+            this.ChkOnTop.UseVisualStyleBackColor = true;
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(68, 192);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.BtnRefresh.TabIndex = 2;
+            this.BtnRefresh.Text = "&Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 450);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.ChkOnTop);
+            this.Controls.Add(this.LstFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Shortcutter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstFiles;
+        private System.Windows.Forms.CheckBox ChkOnTop;
+        private System.Windows.Forms.Button BtnRefresh;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Shortcutter
+{
+    public partial class MainForm : Form
+    {
+        bool isMouseDown = false;
+        Point mouseOffset;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ChkOnTop.Checked = Properties.Settings.Default.FloatOnTop;
+            this.TopMost = ChkOnTop.Checked;
+            this.Location = new Point(Properties.Settings.Default.LocationX, Properties.Settings.Default.LocationY);
+
+            this.Opacity = Properties.Settings.Default.OpacityPercent;
+
+            if (this.Opacity < 10)
+            {
+                this.Opacity = 10;
+            }
+
+            GetFiles();
+
+        }
+
+
+        private void GetFiles()
+        {
+            if (!Directory.Exists(Properties.Settings.Default.ShortcutFolder))
+            {
+                MessageBox.Show("Set a folder And extension in the config file.", "Settings must make sense...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            string[] files = Directory.GetFiles(Properties.Settings.Default.ShortcutFolder, "*." + Properties.Settings.Default.FileExt);
+
+            LstFiles.Items.Clear();
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                LstFiles.Items.Add(fileName);
+            }
+
+            LstFiles.Size = LstFiles.PreferredSize;
+            if (LstFiles.PreferredSize.Width < 200)
+            {
+                LstFiles.Height = LstFiles.PreferredSize.Height;
+                LstFiles.Width = 200;
+            }
+
+            ChkOnTop.Left = 2;
+            ChkOnTop.Top = LstFiles.Height + 5;
+            BtnRefresh.Top = LstFiles.Height + 1;
+
+            this.Size = this.PreferredSize;
+
+
+
+        }
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
